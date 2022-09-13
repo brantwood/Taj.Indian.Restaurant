3e4f3ab9ac43feece124815dd1dbbf1cfa3007ee
B.rant <64997329+brantwood@users.noreply.github.com>
2022-09-13 15:13:35 +1000

    class toggle to review side menu modal

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // hamburger menu animation
 const menuBtn = document.querySelector(".menu-btn");
 
+const sideMenuModal = document.querySelector(".side-menu");
+
+
 menuBtn.addEventListener("click", function(){
-    menuBtn.classList.toggle('open')
+    menuBtn.classList.toggle('open');
+    sideMenuModal.classList.toggle('show-menu');
 })
 
