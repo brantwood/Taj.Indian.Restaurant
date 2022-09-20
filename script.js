fa806d649069845402c2afddc36ef20c3738d9b9
B.rant <64997329+brantwood@users.noreply.github.com>
2022-09-20 18:26:57 +1000

    comments for guidance

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const menuBtn = document.querySelector(".menu-btn");
 
 const sideMenuModal = document.querySelector(".side-menu");
 
-
+// Function toggles the class to an off-screen modal window making it visible
 menuBtn.addEventListener("click", function(){
     menuBtn.classList.toggle('open');
     sideMenuModal.classList.toggle('show-menu');
